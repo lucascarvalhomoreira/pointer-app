bc4b96405e5a22be096b19c403b1c2022db595b6
EC2 Default User <ec2-user@ip-172-31-39-61.ec2.internal>
2023-12-07 23:08:02 +0000

    Add Terraform Files

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "SEU-BUCKET-NAME"
+    key    = "pointer-app.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable vpc_id {
+  type        = string
+  description = "Informe sua VPC ID"
+}
+
+variable subnet_id {
+  type        = string
+  description = "Informe sua subnet id"
+}
